60d68f84979063ae1f04205e8bfce184cd71dd6c
Alieva Marina <furrycun11@gmail.com>
2023-04-17 13:37:34 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,33 @@
 package ru.stqa.pft.sandbox;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Marina");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(1));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
