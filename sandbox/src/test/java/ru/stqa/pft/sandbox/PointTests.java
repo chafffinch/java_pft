b7a79a4a1d49bd6d87c4bbbf3aa395440511bcbd
Alieva Marina <furrycun11@gmail.com>
2023-04-20 18:52:24 +0300

    Задание 3 (point + test)

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class PointTests {
+
+
+    @Test
+    public void testDistance() {
+        Point p1 = new Point(6, 6);
+        Point p2 = new Point(2, 3);
+        Assert.assertEquals(p1.distance(p2), 5.0);
+    }
+}
