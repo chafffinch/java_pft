c423da28c9d2ab98ad6dc719fe6e1f8f487db1e1
Alieva Marina <furrycun11@gmail.com>
2023-04-23 14:34:35 +0300

    Поправки к заданию №3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -3,11 +3,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 public class PointTests {
 
-
     @Test
-    public void testDistance() {
+    public void testDistance_successReturn() {
         Point p1 = new Point(6, 6);
         Point p2 = new Point(2, 3);
         Assert.assertEquals(p1.distance(p2), 5.0);
+        }
+
+    @Test
+    public void testDistance_notsuccessReturn() {
+        Point p1 = new Point(6, 6);
+        Point p2 = new Point(2, 3);
+        Assert.assertEquals(p1.distance(p2), 11.0);
+        }
+
     }
-}
+
