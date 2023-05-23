aacb55166656ad0991a14cc6832a4cce909de1b7
Alieva Marina <furrycun11@gmail.com>
2023-05-23 18:35:20 +0300

    TestBase (задание 5)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,8 +5,6 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
-
 public class TestBase {
 
     protected final ApplicationManager app = new ApplicationManager();
