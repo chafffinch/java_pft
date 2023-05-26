0af25efbb2815c01eaf5bb3aa12c41b4d9ea1ea7
Alieva Marina <furrycun11@gmail.com>
2023-05-26 19:58:37 +0300

    Доработана проверка и обеспечение выполнения предусловий (2)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -2,9 +2,16 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactDeletionTests extends TestBase{
+
   @Test
+   app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+  }
+
   public void testContactDeletion() throws Exception {
     app.getNavigationHelper().gotoHomePage();
     if(! app.getContactHelper().isThereAContact()) {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreationTests.java
@@ -5,10 +5,15 @@ import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class NewContactCreationTests extends TestBase {
   @Test
   public void testNewContactCreation() throws Exception {
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getContactHelper().createContact(new ContactData("test1", "test2", "test3", "test4", "test5"), true);
   }
 
