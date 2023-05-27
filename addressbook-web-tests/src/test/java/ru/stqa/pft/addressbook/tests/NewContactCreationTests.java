333e07b2d28f99e674bd441ae166501db1af1860
Alieva Marina <furrycun11@gmail.com>
2023-05-27 22:27:52 +0300

    Лямбда (контакты)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreationTests.java
@@ -1,27 +1,25 @@
 package ru.stqa.pft.addressbook.tests;
 
-import java.util.concurrent.TimeUnit;
-
 import org.testng.Assert;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.HashSet;
+import java.util.List;
 
 public class NewContactCreationTests extends TestBase {
-  @Test
-  public void testNewContactCreation() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    @Test
+    public void testNewContactCreation() throws Exception {
+        app.getNavigationHelper().gotoHomePage();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        ContactData contact = new ContactData("test1", "test2", "test3", "test4", "test5");
+        app.getContactHelper().createContact(contact, true);
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
-    app.getNavigationHelper().gotoHomePage();
-    int before = app.getContactHelper().getContactCount();
 
-    app.getContactHelper().createContact(new ContactData("test1", "test2", "test3", "test4", "test5"), true);
-
-    int after = app.getContactHelper().getContactCount();   //считаем количество контактов после создания
-    Assert.assertEquals(after, before + 1);   }
-
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+}
 }
