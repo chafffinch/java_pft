1bfa667ed24b2172680a0a5c77797996ab8b6768
Alieva Marina <furrycun11@gmail.com>
2023-06-10 23:53:13 +0300

    Реализованы генераторы тестовых данных (с сохранением данных) (12) 1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -3,9 +3,6 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 
@@ -23,9 +20,6 @@ public class ContactDataGenerator {
     @Parameter (names = "-f", description = "Target file")
     public String file;
 
-    @Parameter (names = "-d", description = "Data format")
-    public String format;
-
     public static void main(String[] args) throws IOException {
         ContactDataGenerator generator = new ContactDataGenerator();
         JCommander jCommander = new JCommander(generator);
@@ -40,42 +34,16 @@ public class ContactDataGenerator {
 
     private void run() throws IOException {
         List<ContactData> contacts = generateContact(count);
-        if (format.equals("csv")) {
-            saveAsCsv(contacts, new File(file));
-        } else if (format.equals("xml")) {
-            saveAsXml(contacts, new File(file));
-        } else if (format.equals("json")) {
-            saveAsJson(contacts, new File(file));
-        } else {
-            System.out.println("Я не знаю такой формат" + format);
-        }
+        save(contacts, new File(file));
     }
 
-    private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
-        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
-        String json = gson.toJson(contacts);
-        try (Writer writer = new FileWriter(file)) {
-            writer.write(json);
+    private void save(List<ContactData> contacts, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s;%s\n", contact.getName(), contact.getFirstname()
+                    , contact.getMobileTelephone(), contact.getGroup()));
         }
-    }
-
-    private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
-        XStream xstream = new XStream();
-        xstream.processAnnotations(ContactData.class);
-        String xml = xstream.toXML(contacts);
-        try (Writer writer = new FileWriter(file)) {
-            writer.write(xml);
-        }
-    }
-
-    private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-        try (Writer writer = new FileWriter(file)) {
-            for (ContactData contact : contacts) {
-                writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getName(), contact.getFirstname()
-                        , contact.getMobileTelephone(), contact.getGroup(), contact.getHomePhone()
-                        , contact.getAddress(), contact.getMail(), contact.getMail2()));
-            }
-        }
+        writer.close();
     }
 
     private List<ContactData> generateContact(int count) {
@@ -83,7 +51,7 @@ public class ContactDataGenerator {
         for (int i = 0; i < count; i++) {
             contacts.add(new ContactData().withName(String.format("name%s", i))
                     .withFirstname(String.format("firstname%s", i))
-                    .withMobileTelephone(String.format("77-834%s", i)).withGroup("test1"));
+                    .withMobileTelephone(String.format("02-3365%s", i)).withGroup("test1"));
         }
         return contacts;
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/NewContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/NewContactCreationTests.java
@@ -1,19 +1,19 @@
 package ru.stqa.pft.addressbook.test;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.thoughtworks.xstream.XStream;
+
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
-import ru.stqa.pft.addressbook.model.Groups;
+import ru.stqa.pft.addressbook.tests.TestBase;
 
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,56 +21,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class NewContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroupsFromXml() throws IOException {
+    public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
-        String xml = "";
+        File photo = new File("src/test/resources/Ferma.png");
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.csv")));
         String line = reader.readLine();
         while (line != null) {
-            xml += line;
+            String[] split = line.split(";");
+            list.add(new Object[]{new ContactData().withName(split[0]).withFirstname(split[1])
+                    .withMobileTelephone(split[2]).withGroup(split[3])});
             line = reader.readLine();
         }
-        XStream xstream = new XStream();
-        xstream.processAnnotations(GroupData.class);
-        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
-        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+        return list.iterator();
     }
 
-    @DataProvider
-    public Iterator<Object[]> validGroupsFromJson() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
-        String json = "";
-        String line = reader.readLine();
-        while (line != null) {
-            json += line;
-            line = reader.readLine();
-        }
-        Gson gson = new Gson();
-        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
-        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
-    }
-
-    @Test(dataProvider = "validGroupsFromJson")
-    public void testGroupCreation(GroupData group) throws Exception {
-        app.goTo().groupPage();
-        Groups before = app.group().all();
-        app.group().create(group);
-        assertThat(app.group().count(), equalTo(before.size() + 1));
-        Groups after = app.group().all();
+    @Test (dataProvider = "validContacts")
+    public void testNewContactCreation(ContactData contact) throws Exception {
+        app.goTo().homePage();
+        Contacts before = app.contact().all();
+        app.contact().create(contact, true);
+        assertThat(app.contact().count(), equalTo(before.size() + 1));
+        Contacts after = app.contact().all();
         assertThat(after, equalTo(
-                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+                before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
-
-    @Test (enabled = false)
-    public void testBadGroupCreation() throws Exception {
-        app.goTo().groupPage();
-        Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2");
-        app.group().create(group);
-        assertThat(app.group().count(), equalTo(before.size()));
-        Groups after = app.group().all();
-        assertThat(after, equalTo(before));
-    }
-
 }
