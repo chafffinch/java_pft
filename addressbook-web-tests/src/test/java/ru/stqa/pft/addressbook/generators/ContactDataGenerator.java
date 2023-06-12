3b60ecc4a1e5c5c3e9cdc32e38ff81823b2bb2ba
Alieva Marina <furrycun11@gmail.com>
2023-06-12 20:23:21 +0300

    JCommander для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,4 +1,58 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactDataGenerator {
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContact(count);
+        save(contacts, new File(file));
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s\n", contact.getName(), contact.getFirstname(), contact.getMobileTelephone()));
+        }
+        writer.close();
+    }
+
+    private List<ContactData> generateContact(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withName(String.format("name %s", i))
+                    .withFirstname(String.format("firstname %s", i))
+                    .withMobileTelephone(String.format("MobileTelephone %s", i)));
+        }
+        return contacts;
+    }
 }
+
