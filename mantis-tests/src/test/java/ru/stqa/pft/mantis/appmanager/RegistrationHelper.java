328d1697f92ba8ffb3292b458babe510d87f9d27
Alieva Marina <furrycun11@gmail.com>
2023-06-15 15:47:08 +0300

    Сделан файл testng.xml для тестов мантиса

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,24 +1,17 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper extends HelperBase{
+public class RegistrationHelper {
+    private final ApplicationManager app;
+    private WebDriver wd;
 
     public RegistrationHelper(ApplicationManager app) {
-        super(app);
+        this.app = app;
+        wd = app.getDriver();
     }
 
     public void start(String username, String email) {
         wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
-        type(By.name("username"), username);
-        type(By.name("email"), email);
-        click(By.cssSelector("input[value='Signup']")); // Ищем input у которого атрибут value равен нужному
-    }
-
-    public void finish(String confirmationLink, String password) {
-        wd.get(confirmationLink);
-        type(By.name("password"), password);
-        type(By.name("password_confirm"), password);
-        click(By.cssSelector("button[type='submit']"));
     }
 }
