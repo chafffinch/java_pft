62b956b91c15b66d5289a56fd067b8ef60c4f298
Alieva Marina <furrycun11@gmail.com>
2023-06-15 15:20:06 +0300

    №17 Добавлен новый модуль для тестов для Mantis

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -12,16 +12,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.MatchResult;
 
 public class ApplicationManager {
     private final Properties properties;
-    private WebDriver wd;
+    WebDriver wd;
 
     private String browser;
-    private RegistrationHelper registrationHelper;
-    private FtpHelper ftp ;
-    private MailHelper mailHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -31,56 +27,20 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
     }
+
 
     public void stop() {
-        if (wd != null) {
-            wd.quit();
-        }
-    }
-
-
-    public HttpSession newSession() {
-        return new HttpSession(this);
-    }
-
-    public String getProperty(String key) {
-        return properties.getProperty(key);
-    }
-
-    public RegistrationHelper registration() {
-        if (registrationHelper == null) {
-            registrationHelper = new RegistrationHelper(this);
-        }
-        return registrationHelper;
-    }
-
-    public FtpHelper ftp() {
-        if (ftp == null) {
-            ftp = new FtpHelper(this);
-        }
-        return ftp;
-    }
-
-    public WebDriver getDriver() {
-        if (wd == null) {
-            if (browser.equals(BrowserType.FIREFOX)) {
-                wd = new FirefoxDriver();
-            } else if (browser.equals(BrowserType.CHROME)) {
-                wd = new ChromeDriver();
-            } else if (browser.equals(BrowserType.IE)) {
-                wd = new InternetExplorerDriver();
-            }
-            wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            wd.get(properties.getProperty("web.baseUrl"));
-        }
-        return wd;
-    }
-
-    public MailHelper mail() {
-        if (mailHelper == null) {
-            mailHelper = new MailHelper(this);
-        }
-        return mailHelper;
+        wd.quit();
     }
 }
